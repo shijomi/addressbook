ae3ae57d0a55c029d8b77bdff4715c1c65068358
Shijomi Nagashiro <shijominagashiroq@gmail.com>
2019-08-24 11:02:59 -0400

    intermedio

---
+++ mvcAddress/mvcAddress/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿
+namespace mvcAddress.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        instagram,
+        dblink
+    }
+    public class Book
+    { 
+        [Key]
+        public int bookID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int MyProperty { get; set; }
+        [Required]
+        public TypeContact type { get; set; }
+        [Required]
+        public string contact { get; set; }
+
+    }
+}
---
+++ mvcAddress/mvcAddress/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace mvcAddress.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+        
+    }
+}

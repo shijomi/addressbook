48cb3f17d2a1368c2d5acb2a3f83a27ddc379566
nicolasdelgadillos <nicolasdelgadillo75@gmail.com>
2019-08-31 09:51:51 -0400

    web api ok

---
+++ mvcAddress/apiAddressbook/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace apiAddressbook.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiAddressbook.Models.Book> Books { get; set; }
+    }
+}
